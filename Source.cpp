5043743f3845c5a2f30e44e891238db52c9890b2
Ahmed Al-naggar <alnaggar98@hotmail.com>
2023-10-12 17:58:36 +0300

    addind comments

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,15 @@ void main() {
 	// this is my first CPP Program
 	int num1, num2;
 
+	// read first number
 	cout << "Enter Num1 value: ";
 	cin >> num1;
 
+	// read second number
 	cout << "Enter Num2 value: ";
 	cin >> num2;
 
+	// apply operations
 	cout << "Num1 + Num2 = " << num1 + num2 << '\n';
 	cout << "Num1 - Num2 = " << num1 - num2 << '\n';
 	cout << "Num1 * Num2 = " << num1 * num2 << '\n';
